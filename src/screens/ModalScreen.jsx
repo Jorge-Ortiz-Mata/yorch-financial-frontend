fbce7913355562873ff4902a96270f0529184d1e
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-05-09 00:35:22 -0500

    Adding and finishing modal.

--- src/screens/ModalScreen.jsx
+++ src/screens/ModalScreen.jsx
@@ -1,9 +1,20 @@
-import { View } from "react-native"
+import { StatusBar } from 'expo-status-bar';
+import { View, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const ModalScreen = () => {
 
   return(
-    <View></View>
+      <LinearGradient
+        colors={['#000000', '#212A3E']}
+        className="flex-1 items-center justify-center"
+      >
+        <MaterialCommunityIcons name="finance" size={100} color="white" />
+        <Text className="text-white font-black text-4xl mt-5">Yorch Financial</Text>
+        <Text className="text-white font-semibold my-1 text-lg">El control de tus ingresos y gastos</Text>
+        <StatusBar style="light" />
+      </LinearGradient>
   )
 }
 
