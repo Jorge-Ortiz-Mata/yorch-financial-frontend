e2b17c0482add25528f65f8a5f329f01b091306f
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-05-12 17:48:09 -0500

    Selecting Date on the Edit Profile screen.

---
+++ src/components/common/CustomDatePicker.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { View, Text } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import CustomTextLabel from './CustomTextLabel';
+
+const CustomDatePicker = () => {
+  const [dateSelected, setDateSelected] = useState(new Date(1683931385416));
+  const [displatyDate, setDisplatyDate] = useState(false);
+
+  const openDatePicker = () => {
+    setDisplatyDate(true)
+  }
+
+  const handleChange = (event) => {
+    setDisplatyDate(false);
+    const dateEntered = event.nativeEvent.timestamp;
+    setDateSelected(new Date(dateEntered));
+  };
+
+  return (
+    <View>
+      <CustomTextLabel title="Fecha de nacimiento" />
+      <Text
+        onPress={openDatePicker}
+        className={`bg-gray-100 text-gray-800 font-bold ${ Platform.OS === 'ios' ? "py-5" : "py-3" } px-2 rounded mb-5`}
+      >
+        {dateSelected.toLocaleDateString()}
+      </Text>
+      {displatyDate && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={dateSelected}
+          mode={'date'}
+          is24Hour={true}
+          onChange={handleChange}
+        />
+      )}
+    </View>
+  );
+};
+
+export default CustomDatePicker;
--- src/components/editProfileScreen/EditProfileForm.jsx
+++ src/components/editProfileScreen/EditProfileForm.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import { View } from "react-native";
+
 import CustomTextInput from "../common/CustomTextInput";
 import CustomTextLabel from "../common/CustomTextLabel";
 import CustomButton from "../common/CustomButton";
 import CustomUserForm from "../common/CustomUserForm";
-import { useState } from "react";
 import CustomImagePicker from "../common/CustomImagePicker";
+import CustomDatePicker from "../common/CustomDatePicker";
 
 const initialState = { name: '', paternal_surname: '', maternal_surname: '', sex: '', date_of_birth: '', avatar: '' }
 
@@ -29,42 +31,38 @@ const EditProfileForm = () => {
           onSelectImage={handleChange}
         />
       </View>
-      <>
-        <CustomTextLabel title="Nombre(s)" />
-        <CustomTextInput
-          name="name"
-          value={formParams.name}
-          placeholder="José Luis"
-          secureTextEntry={false}
-          onChange={handleChange}
-        />
-      </>
-      <>
-        <CustomTextLabel title="Apellido Paterno" />
-        <CustomTextInput
-          name="paternal_surname"
-          value={formParams.paternal_surname}
-          placeholder="Pérez"
-          secureTextEntry={false}
-          onChange={handleChange}
-        />
-      </>
-      <>
-        <CustomTextLabel title="Apellido Materno" />
-        <CustomTextInput
-          name="maternal_surname"
-          value={formParams.maternal_surname}
-          placeholder="López"
-          secureTextEntry={false}
-          onChange={handleChange}
-        />
-      </>
-      <>
-        <CustomTextLabel title="Sexo" />
-      </>
-      <>
-        <CustomTextLabel title="Fecha de nacimiento" />
-      </>
+
+      <CustomTextLabel title="Nombre(s)" />
+      <CustomTextInput
+        name="name"
+        value={formParams.name}
+        placeholder="José Luis"
+        secureTextEntry={false}
+        onChange={handleChange}
+      />
+
+      <CustomTextLabel title="Apellido Paterno" />
+      <CustomTextInput
+        name="paternal_surname"
+        value={formParams.paternal_surname}
+        placeholder="Pérez"
+        secureTextEntry={false}
+        onChange={handleChange}
+      />
+
+      <CustomTextLabel title="Apellido Materno" />
+      <CustomTextInput
+        name="maternal_surname"
+        value={formParams.maternal_surname}
+        placeholder="López"
+        secureTextEntry={false}
+        onChange={handleChange}
+      />
+
+      <CustomTextLabel title="Sexo" />
+
+      <CustomDatePicker />
+
       <View className="items-center">
         <CustomButton
           title="Actualizar perfil"
